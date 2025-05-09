47520dbfce3c637d42265464e4ec7d871672549f
tiagofelp <tiago.alves1403@outlook.com>
2025-05-08 21:49:12 -0300

    Delete api directory

--- api/sair.php
+++
@@ -1,9 +0,0 @@
-<?php 
-		if (!session_start()) {
-			die("Não foi possível abrir sessão!");
-		}
-		unset($_SESSION['id']);
-			session_destroy();
-			ob_clean();
-			header("Location: index.php");
-	 ?>
